1505784d2dd7a1edbcddf7c6459e81550e34aefe
aaronchn99 <36250963+aaronchn99@users.noreply.github.com>
2024-01-04 01:30:14 +0000

    Refactor(Brick):Local to Scene activated at scene level instead of script - This ensures texture/gradient resources are made unique, preventing color changes affecting all other bricks in game

--- Scripts/brick.gd
+++ Scripts/brick.gd
@@ -9,8 +9,7 @@ var breakable = true :
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$Sprite.texture.set_local_to_scene(true)
-	$Sprite.texture.gradient.set_local_to_scene(true)
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
